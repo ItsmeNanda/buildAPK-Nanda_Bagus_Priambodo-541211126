004deca3e330e1735a8e2f5e621ed56882e49053
itsmeNanda <nanda.bagus2006@gmail.com>
2024-01-10 15:19:52 +0700

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hallo World!</Text>
+      <Text>Aku Nanda, Dari SMK Telkom Purwokerto</Text>
+      <Text>Senang bertemu dengan kamu</Text>
       <StatusBar style="auto" />
     </View>
   );
